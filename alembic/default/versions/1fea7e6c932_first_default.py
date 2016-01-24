a137d40a294fb49d5c49b6fabe688dce424ffc8d
TakesxiSximada <takesxi.sximada@gmail.com>
2016-01-24 23:20:56 +0900

    add alembic

---
+++ alembic/default/versions/1fea7e6c932_first_default.py
@@ -0,0 +1,24 @@
+"""first default
+
+Revision ID: 1fea7e6c932
+Revises: 
+Create Date: 2016-01-23 17:43:13.046771
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1fea7e6c932'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/other/versions/517bc86ab75_first_other.py
@@ -0,0 +1,25 @@
+"""first other
+
+
+Revision ID: 517bc86ab75
+Revises: 
+Create Date: 2016-01-23 17:43:26.225544
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '517bc86ab75'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
